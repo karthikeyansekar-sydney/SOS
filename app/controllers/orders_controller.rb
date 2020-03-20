77702aa513720794636f36d3a2530bfec1fd1c75
karthikeyansekar <karthikeyansep27@gmail.com>
2020-03-20 11:30:50 +1100

    SOS - CSS fixed version

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,15 @@ class OrdersController < ApplicationController
     def create
     @employee = Employee.find(session[:user_id])
     @order = @employee.orders.create(order_params)
-redirect_to root_path
-end
+    redirect_to root_path
+    end
 
 def index
   @orders = Order.all
 end
 
 def show
- @employee = Employee.find(session[:user_id])
+  @employee = Employee.find(session[:user_id])
   @orders = Order.where((["employee_id = ?", session[:user_id]]))
 end
 
